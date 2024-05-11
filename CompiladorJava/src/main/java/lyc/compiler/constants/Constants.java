a2eff454a0fad10e18d8c0e99ff257ea4ed9e64c
LucasSuriano <70494844+LucasSuriano@users.noreply.github.com>
2024-05-10 21:56:45 -0300

    Compilador v1

---
+++ CompiladorJava/src/main/java/lyc/compiler/constants/Constants.java
@@ -0,0 +1,11 @@
+package lyc.compiler.constants;
+
+public final class Constants {
+
+    public static final int MAX_LENGTH = 40;
+    public static final int MAX_INT_16 = 65535;
+    //public static final int MAX_FLOAT_32 = TO DO ?
+
+    private Constants(){}
+
+}
